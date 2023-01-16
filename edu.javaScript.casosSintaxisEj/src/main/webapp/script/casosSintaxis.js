55203d734056af0820e33a990ec9e40d0df21592
DESKTOP-USHSCCC\csi-r <csi22-rcoriaz@colegioaltair.net>
2023-01-16 14:10:06 +0100

    no message

---
+++ edu.javaScript.casosSintaxisEj/src/main/webapp/script/casosSintaxis.js
@@ -0,0 +1,50 @@
+function caso1(){
+	var a = 5;
+var b = 10;
+
+if (a === 5) {
+  let a = 4;
+  var b = 1;
+
+  
+
+document.write(a);
+  
+
+document.write(b);
+}
+
+document.write(a);
+document.write(b);
+}
+
+function caso2(){
+	for (let i = 0; i<10; i++) {
+  
+
+document.write(i);
+}
+
+document.write(i);
+}
+
+function caso3(){
+var x = 5;
+var y = 0;
+/*
+let (x = x+10, y = 12) {
+  
+
+document.write(x+y);
+}
+*/
+document.write(x + y);
+}
+
+function caso4(){
+var a = 5;
+//let(a = 6) 
+
+document.write(a);
+document.write(a);
+}
